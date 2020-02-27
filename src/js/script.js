e4e0bbc58a099e256a3b734329f3d37c4d236e72
Yaroslav Ekimov <YaroslavMur@mail.ru>
2020-02-27 13:07:08 +0300

    finish

--- src/js/script.js
+++ src/js/script.js
@@ -1,28 +1,28 @@
-// window.addEventListener('DOMContentLoaded', () => {
-//     const menu = document.querySelector('.menu'),
-//     menuItem = document.querySelectorAll('.menu_item'),
-//     hamburger = document.querySelector('.hamburger');
+window.addEventListener('DOMContentLoaded', () => {
+    const menu = document.querySelector('.nav__menu'),
+    menuItem = document.querySelectorAll('.nav__list'),
+    hamburger = document.querySelector('.hamburger');
 
-//     hamburger.addEventListener('click', () => {
-//         hamburger.classList.toggle('hamburger_active');
-//         menu.classList.toggle('menu_active');
-//     });
+    hamburger.addEventListener('click', () => {
+        hamburger.classList.toggle('hamburger_active');
+        menu.classList.toggle('nav__menu_active');
+    });
 
-//     menuItem.forEach(item => {
-//         item.addEventListener('click', () => {
-//             hamburger.classList.toggle('hamburger_active');
-//             menu.classList.toggle('menu_active');
-//         })
-//     })
-// })
+    menuItem.forEach(item => {
+        item.addEventListener('click', () => {
+            hamburger.classList.toggle('hamburger_active');
+            menu.classList.toggle('nav__menu_active');
+        })
+    })
+})
 
 $(document).ready(function(){
     $('.carousel__item').slick({
         speed: 1200,
         adaptiveHeight: false,
         autoplay: true,
-        prevArrow: '<button type="button" class="slick-prev"><img src="../icon/reviews/reviews_prew.svg"></button>',
-        nextArrow: '<button type="button" class="slick-next"><img src="../icon/reviews/reviews_next.svg"></button>',
+        prevArrow: '<button type="button" class="slick-prev"><img src="icon/reviews/reviews_prew.svg"></button>',
+        nextArrow: '<button type="button" class="slick-next"><img src="icon/reviews/reviews_next.svg"></button>',
         responsive: [
             {
                 breakpoint: 992,
@@ -42,7 +42,7 @@ $(document).ready(function(){
                 }
             },
             {
-                breakpoint: 576,
+                breakpoint: 600,
                 settings: {
                     speed: 1000,
                     dots: false,
