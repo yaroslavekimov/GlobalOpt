ecd3e0b0939f9e382d659646d12673f62c4678de
Yaroslav Ekimov <YaroslavMur@mail.ru>
2020-03-01 13:38:57 +0300

    GOOD

--- dist/js/script.js
+++ dist/js/script.js
@@ -94,39 +94,22 @@ $(document).ready(function(){
         return false;
     });
 
-    // Smooth scroll and pageup
+    
+// // Smooth scroll and pageup
 
-    $(window).scroll(function() {
-        if ($(this).scrollTop() > 1600) {
-            $('.pageup').fadeIn();
-        } else {
-            $('.pageup').fadeOut();
-        }
-    });
-
-    $("a[href^='#']").click(function(){
-        const _href = $(this).attr("href");
-        $("html, body").animate({scrollTop: $(_href).offset().top+"px"});
-        return false;
-    });
+//     $(window).scroll(function() {
+//         if ($(this).scrollTop() > 1600) {
+//             $('.pageup').fadeIn();
+//         } else {
+//             $('.pageup').fadeOut();
+//         }
+//     });
+    
+//     $("a[href^='#']").click(function(){
+//         const _href = $(this).attr("href");
+//         $("html, body").animate({scrollTop: $(_href).offset().top+"px"});
+//         return false;
+//     });
 
     new WOW().init();
 });
-
-window.addEventListener('DOMContentLoaded', () => {
-    const menu = document.querySelector('.menu'),
-    menuItem = document.querySelectorAll('.menu_item'),
-    hamburger = document.querySelector('.hamburger');
-
-    hamburger.addEventListener('click', () => {
-        hamburger.classList.toggle('hamburger_active');
-        menu.classList.toggle('menu_active');
-    });
-
-    menuItem.forEach(item => {
-        item.addEventListener('click', () => {
-            hamburger.classList.toggle('hamburger_active');
-            menu.classList.toggle('menu');
-        })
-    })
-})
